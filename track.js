196576bb09db1440c637da2c4aa1a6f8dadfab8a
Erin Ranta <erinran99@gmail.com>
2022-06-23 20:18:10 -0400

    finally fixed img

--- track.js
+++ track.js
@@ -1,22 +1,16 @@
 'use strict';
 
-
-let imageElementsArray = document.querySelectorAll('img'); // [<img>]
-console.log(imageElementsArray);
+let imageElements = document.querySelectorAll('img');
+console.log(imageElements);
 
 let clicks = 0;
 let views = 0;
+let roundTracker = 0;
 
-// looping through imageElementsArray
-// for (let i = 0; i < imageElementsArray.length; i++) {
-//   let img = imageElementsArray[i];
-//   img.src = './assets/image.jpeg';
-}
-
-let imageFiles = [
+let fileNames = [
   'bag.jpg',
-  'bathroom.jpg',
   'banana.jpg',
+  'bathroom.jpg',
   'boots.jpg',
   'breakfast.jpg',
   'bubblegum.jpg',
@@ -28,103 +22,107 @@ let imageFiles = [
   'pet-sweep.jpg',
   'scissors.jpg',
   'shark.jpg',
-  'sweep.png',
+  'sweep.jpg',
   'tauntaun.jpg',
   'unicorn.jpg',
   'water-can.jpg',
-  'wine-glass.jpg'
 ];
 
-let numImages = imageFiles.length; 
-let numVisible = 3; 
-
-Image.prototype.removeExtension = function() {
-  let label = this.fileName.slice(0,this.fileName.indexOf('.'));
-  return(label);
-};
 
 const images = [];
 
-
-function Image(fileName) {
+function Product(fileName) {
+  this.id = fileName;
   this.clicks = 0;
   this.views = 0;
-  this.id = fileName;
-  this.src = `./assets/images/${fileName}`;
+  this.src = `./img/${fileName}`;
 }
 
-
-Image.prototype.handleClick = function() {
-
+Product.prototype.handleClick = function() {
 };
 
-
-for (let i = 0; i < imageFiles.length; i++) {
-  images.push(new Image(imageFiles[i]));
+for (let i = 0; i < fileNames.length; i++) {
+  images.push(new Product(fileNames[i]));
 }
 
-
-imageElementsArray[0].id = images[0].id;
-imageElementsArray[0].src = images[0].src;
+imageElements[0].id = images[0].id;
+imageElements[0].src = images[0].src;
 images[0].views++;
-imageElementsArray[1].id = images[1].id;
-imageElementsArray[1].src = images[1].src;
+imageElements[1].id = images[1].id;
+imageElements[1].src = images[1].src;
 images[1].views++;
+imageElements[2].id = images[2].id;
+imageElements[2].src = images[2].src;
+images[2].views++;
 
 
+
+ 
 function handleClick(event) {
-  
-  for (let i = 0; i < images.length; i++) { 
+  for (let i = 0; i <images.length; i++) {
     console.log(event.target.id, images[i].id);
     if (event.target.id === images[i].id) {
       images[i].clicks++;
     }
   }
+  if (roundTracker === 25) {
+    let imageElement = document.getElementById('image-selection');
+    imageElement.innerHTML = 'Voting Has Ended, Thank You!';
+    return;
+  }
   renderImages();
   console.log(images);
+  roundTracker++;
+  console.log(roundTracker);
 }
 
-/
-imageElementsArray.forEach(function (img) {
+imageElements.forEach(function (img){
   img.addEventListener('click', handleClick);
-}); 
-
+});
 
 function renderImages() {
-
-  
   let image1 = generateRandomImage();
   let image2 = generateRandomImage();
-//   let image3 = generateRandomImage();
+  let image3 = generateRandomImage();
 
-  while (image1.id === image2.id) {
+  while ((image1.id === image2.id) || (image1.id === image3.id)) {
     image1 = generateRandomImage();
   }
+  while (image2.id === image3.id) {
+    image2 = generateRandomImage();
+  }
 
-  
-  imageElementsArray[0].id = image1.id;
-  imageElementsArray[0].src = image1.src;
-  imageElementsArray[1].id = image2.id;
-  imageElementsArray[1].src = image2.src;
-  imageElementsArray[2].src = image3.id;
-  imageElementsArray[2].src = image3.src;
+  imageElements[0].id = image1.id;
+  imageElements[0].src = image1.src;
   image1.views++;
+  imageElements[1].id = image2.id;
+  imageElements[1].src = image2.src;
   image2.views++;
+  imageElements[2].id = image3.id;
+  imageElements[2].src = image3.src;
   image3.views++;
 }
 
-
-function generateRandomNumbers() {
-  let indices = [];
-  let index1 = Math.floor(Math.random() * numImages);
-  let index2 = Math.floor(Math.random() * numImages);
-  let index3 = Math.floor(Math.random() * numImages);
-
-  while(index1 === index2 || index1 === index3){
-    let index1 = Math.floor(Math.random() * numImages);
-  }
-  indices.push(index1,index2,index3);
-  console.log(indices);
+function generateRandomImage() {
+  let index = Math.floor(Math.random() * images.length);
+  return images[index];
 }
 
-console.log(images);
+Product.prototype.renderResults = function() {
+  const parentElement = document.getElementById('results-table');
+  const article = document.createElement('article');
+  parentElement.appendChild(article);
+
+  const h2 = document.createElement('h2');
+  h2.textContent = this.id + ' had ' + this.clicks + ' clicks and was viewed ' + this.views + ' times';
+  article.appendChild(h2);
+};
+
+let buttonEl = document.getElementById('results-button');
+
+buttonEl.addEventListener('click', function() {
+  console.log(images);
+  for (let i = 0; i < images.length; i++) {
+    images[i].renderResults();
+  }
+})
